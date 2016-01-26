e7b9220d71ee03cf3c782e25b92558f212bce015
Nekotekina <nekotekina@gmail.com>
2016-01-26 21:13:36 +0300

    PSF Loader simplified

--- rpcs3/Loader/PSF.h
+++ rpcs3/Loader/PSF.h
@@ -1,125 +1,68 @@
 #pragma once
 
-struct vfsStream;
-
 namespace psf
 {
-	enum class entry_format : u16
+	enum class format : u16
 	{
-		unknown = 0,
-		string_not_null_term = 0x0004,
-		string = 0x0204,
+		array   = 0x0004, // claimed to be a non-NTS string (char array)
+		string  = 0x0204,
 		integer = 0x0404,
 	};
 
-	struct header
+	class entry final
 	{
-		u32 magic;
-		u32 version;
-		u32 off_key_table;
-		u32 off_data_table;
-		u32 entries_num;
-	};
-
-	struct def_table
-	{
-		u16 key_off;
-		entry_format param_fmt;
-		u32 param_len;
-		u32 param_max;
-		u32 data_off;
-	};
-
-	class entry
-	{
-		entry_format m_format = entry_format::unknown;
-		u32 m_max_size = 0;
-
-		u32 m_value_integer;
+		u32 m_value_integer = 0; // TODO: is it really unsigned?
 		std::string m_value_string;
 
 	public:
-		u32 max_size() const;
-		entry& max_size(u32 value);
-		entry_format format() const;
-		entry& format(entry_format value);
-		std::string as_string() const;
+		const format type; // Entry format (specified in the constructor)
+		const u32 max_size; // Entry max size (supplementary info, stored in PSF format)
+
+		// Construct string entry, assign the value
+		entry(format type, u32 max_size, const std::string& value = {})
+			: type(type)
+			, max_size(max_size)
+		{
+			CHECK_ASSERTION(type == format::string || type == format::array);
+			CHECK_ASSERTION(max_size);
+			m_value_string = value;
+		}
+
+		// Construct integer entry, assign the value
+		entry(u32 value)
+			: type(format::integer)
+			, max_size(sizeof(u32))
+		{
+			m_value_integer = value;
+		}
+
+		entry(const entry&) = default;
+		entry(entry&&) = default;
+
+		entry& operator =(const entry&) = default;
+		entry& operator =(entry&&) = default;
+
+		const std::string& as_string() const;
 		u32 as_integer() const;
-		std::string to_string() const;
-		u32 to_integer() const;
-		entry& value(const std::string &value_);
-		entry& value(u32 value_);
-		entry& operator = (const std::string &value_);
-		entry& operator = (u32 value_);
 
-		std::size_t size() const;
+		entry& operator =(const std::string& value);
+		entry& operator =(u32 value);
+
+		u32 size() const;
 	};
 
-	class object
-	{
-		std::unordered_map<std::string, entry> m_entries;
+	// Define PSF registry as a sorted map of entries:
+	using registry = std::map<std::string, entry>;
 
-	public:
-		object() = default;
+	// Load PSF registry from binary data
+	registry load(const std::vector<char>&);
 
-		object(vfsStream& stream)
-		{
-			load(stream);
-		}
+	// Convert PSF registry to binary format
+	std::vector<char> save(const registry&);
 
-		virtual ~object() = default;
+	// Get string value or default value
+	std::string get_string(const registry& psf, const std::string& key, const std::string& def = {});
 
-		bool load(vfsStream& stream);
-		bool save(vfsStream& stream) const;
-		void clear();
-
-		explicit operator bool() const
-		{
-			return !m_entries.empty();
-		}
-
-		entry& operator[](const std::string &key);
-		const entry& operator[](const std::string &key) const;
-
-		//returns pointer to entry or null, if not exists
-		const entry *get(const std::string &key) const;
-
-		std::string get_string_or(const std::string &key, const std::string &default_value) const;
-		u32 get_integer_or(const std::string &key, u32 default_value) const;
-
-		bool exists(const std::string &key) const
-		{
-			return m_entries.find(key) != m_entries.end();
-		}
-
-		std::unordered_map<std::string, entry>& entries()
-		{
-			return m_entries;
-		}
-
-		const std::unordered_map<std::string, entry>& entries() const
-		{
-			return m_entries;
-		}
-
-		std::unordered_map<std::string, entry>::iterator begin()
-		{
-			return m_entries.begin();
-		}
-
-		std::unordered_map<std::string, entry>::iterator end()
-		{
-			return m_entries.end();
-		}
-
-		std::unordered_map<std::string, entry>::const_iterator begin() const
-		{
-			return m_entries.begin();
-		}
-
-		std::unordered_map<std::string, entry>::const_iterator end() const
-		{
-			return m_entries.end();
-		}
-	};
+	// Get integer value or default value
+	u32 get_integer(const registry& psf, const std::string& key, u32 def = 0);
 }
